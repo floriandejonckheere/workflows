8d0e640b7eee101f9cf028557a34a03edb194e0c
Florian Dejonckheere <florian@floriandejonckheere.be>
2026-03-09 18:28:44 +0100

    Initial commit

---
+++ config/inflections.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+Workflows.loader.inflector.inflect
---
+++ lib/workflows.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "zeitwerk"
+
+module Workflows
+  class << self
+    # Code loader instance
+    attr_reader :loader
+
+    def root
+      @root ||= Pathname.new(File.expand_path(File.join("..", ".."), __FILE__))
+    end
+
+    def setup
+      @loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: false)
+
+      # Register inflections
+      require root.join("config/inflections.rb")
+
+      # Load types
+      require root.join("lib/workflows/resource_type")
+
+      loader.setup
+      loader.eager_load
+    end
+  end
+end
+
+Workflows.setup
